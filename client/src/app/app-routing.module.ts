d3d56bd7f70fa7ab2ea9dcd1881d46eab4d2076b
Lucille C <114987982+lucillctj@users.noreply.github.com>
2023-04-08 17:28:08 +0200

    add component: create account customer form

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
+import {SignUpComponent} from "./pages/sign-up/sign-up.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'sign-up', component: SignUpComponent },
 
 ];
 
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { CreateAccountComponent } from './create-account/create-account.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CreateAccountComponent } from './create-account-customer/create-account.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
