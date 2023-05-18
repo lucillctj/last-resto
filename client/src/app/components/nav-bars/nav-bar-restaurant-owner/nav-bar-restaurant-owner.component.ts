001f3e80f02552a85daa0c04e1700ca3964a9193
Lucille C <114987982+lucillctj@users.noreply.github.com>
2023-05-18 18:48:22 +0200

    update nav-bar for restaurants

--- client/src/app/components/nav-bars/nav-bar-restaurant-owner/nav-bar-restaurant-owner.component.ts
+++ client/src/app/components/nav-bars/nav-bar-restaurant-owner/nav-bar-restaurant-owner.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import {AuthService} from "../../../services/auth.service";
 
@@ -7,17 +7,26 @@ import {AuthService} from "../../../services/auth.service";
   templateUrl: './nav-bar-restaurant-owner.component.html',
   styleUrls: ['./nav-bar-restaurant-owner.component.scss', '../../../../styles.scss']
 })
-export class NavBarRestaurantOwnerComponent {
-  isSearchActive: boolean;
-  isDashboardActive: boolean;
+export class NavBarRestaurantOwnerComponent implements OnInit{
+  isRestaurantDashboardActive: boolean;
+  isUserDashboardActive: boolean;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private authService: AuthService
   ) {
-    this.isSearchActive = false;
-    this.isDashboardActive = false;
+    this.isRestaurantDashboardActive = false;
+    this.isUserDashboardActive = false;
+  }
+
+  ngOnInit(){
+    if (this.router.url.includes('/api/v1/restaurants/dashboard/')) {
+      this.isRestaurantDashboardActive = true;
+    }
+    else if (this.router.url.includes('/api/v1/restaurant-owners/dashboard')) {
+      this.isUserDashboardActive = true;
+    }
   }
 
   // redirectToReservationsDashboard() {
@@ -34,25 +43,23 @@ export class NavBarRestaurantOwnerComponent {
 
   redirectToRestaurantDashboard() {
     const currentRestaurantId = this.authService.getCurrentRestaurantId();
-console.log(currentRestaurantId)
-    const redirectUrl = `/api/v1/restaurants/dashboard/${currentRestaurantId}`;
-    if (this.router.url === redirectUrl) {
-      this.isSearchActive = true;
+    const urlRestaurantDashboard = `/api/v1/restaurants/dashboard/${currentRestaurantId}`;
+
+    if (this.router.url === urlRestaurantDashboard) {
+      return
     } else {
-      this.isSearchActive = false;
-      this.router.navigate([redirectUrl]);
+      this.router.navigate([urlRestaurantDashboard]);
     }
   }
 
   redirectToUserDashboard() {
     const currentUserId = this.authService.getCurrentUserId();
-    const redirectUrl = `/api/v1/restaurant-owners/dashboard/${currentUserId}`;
-    if (this.router.url === redirectUrl) {
-      this.isDashboardActive = true;
+    const urlUserDashboard = `/api/v1/restaurant-owners/dashboard/${currentUserId}`;
 
+    if (this.router.url === urlUserDashboard) {
+      return
     } else {
-      this.isDashboardActive = false;
-      this.router.navigate([redirectUrl]);
+      this.router.navigate([urlUserDashboard]);
     }
   }
 }
